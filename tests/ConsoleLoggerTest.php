d43d998940753642d13a6997104aada9ddd750b5
Валерий Четков <chetkov.valeriy@gmail.com>
2018-09-21 19:47:22 +0300

    tests

---
+++ tests/ConsoleLoggerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Chetkov\ConsoleLogger;
+
+use Chetkov\ConsoleLogger\ConsoleLogger;
+use Chetkov\ConsoleLogger\Logger;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ConsoleLoggerTest
+ * @package Tests\Chetkov\ConsoleLogger
+ */
+class ConsoleLoggerTest extends TestCase
+{
+    private const MESSAGE = 'Тест';
+
+    /**
+     * @var
+     */
+    private $consoleLogger;
+
+    /**
+     * ConsoleLoggerTest constructor.
+     * @param null|string $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->consoleLogger = new ConsoleLogger();
+    }
+
+    public function testDebug(): void
+    {
+        $this->consoleLogger->debug(self::MESSAGE);
+        $this->expectOutputString($this->generateMessage(Logger::LEVEL_DEBUG));
+    }
+
+    public function testInfo(): void
+    {
+        $this->consoleLogger->info(self::MESSAGE);
+        $this->expectOutputString($this->generateMessage(Logger::LEVEL_INFO));
+    }
+
+    public function testWarning(): void
+    {
+        $this->consoleLogger->warning(self::MESSAGE);
+        $this->expectOutputString($this->generateMessage(Logger::LEVEL_WARNING));
+    }
+
+    public function testError(): void
+    {
+        $this->consoleLogger->error(self::MESSAGE);
+        $this->expectOutputString($this->generateMessage(Logger::LEVEL_ERROR));
+    }
+
+    public function testCritical(): void
+    {
+        $this->consoleLogger->critical(self::MESSAGE);
+        $this->expectOutputString($this->generateMessage(Logger::LEVEL_CRITICAL));
+    }
+
+    /**
+     * @param string $level
+     * @return string
+     */
+    private function generateMessage(string $level): string
+    {
+        $data = [];
+        $currentDateTime = new \DateTime();
+        return implode(' :: ', [
+                $currentDateTime->format('Y-m-d H:i:s'),
+                $level,
+                trim(self::MESSAGE),
+                json_encode($data)
+            ]) . PHP_EOL;
+    }
+}
---
+++ tests/StyledLoggerDecoratorTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Chetkov\ConsoleLogger;
+
+use Chetkov\ConsoleLogger\ConsoleLogger;
+use Chetkov\ConsoleLogger\StyledLogger\LevelStyle;
+use Chetkov\ConsoleLogger\StyledLogger\LoggerStyle;
+use Chetkov\ConsoleLogger\StyledLogger\StyledLoggerDecorator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class StyledLoggerDecoratorTest
+ * @package Tests\Chetkov\ConsoleLogger
+ */
+class StyledLoggerDecoratorTest extends TestCase
+{
+    private const MESSAGE = 'Тест';
+    private const EXPECTED_REGEXP = '/[\s\S]*' . self::MESSAGE . '[\s\S]*/';
+
+    /**
+     * @var StyledLoggerDecorator
+     */
+    private $styledLogger;
+
+    /**
+     * StyledLoggerDecoratorTest constructor.
+     * @param null|string $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $logger = new ConsoleLogger();
+        $loggerStyle = new LoggerStyle();
+        $this->styledLogger = new StyledLoggerDecorator($logger, $loggerStyle);
+    }
+
+    public function test__constructor(): void
+    {
+        $levelStyle = new LevelStyle();
+        $loggerStyle = new LoggerStyle();
+        $loggerStyle->setDebugStyle($levelStyle);
+        $loggerStyle->setInfoStyle($levelStyle);
+        $loggerStyle->setWarningStyle($levelStyle);
+        $loggerStyle->setErrorStyle($levelStyle);
+        $loggerStyle->setCriticalStyle($levelStyle);
+        $styledLogger = new StyledLoggerDecorator(new ConsoleLogger(), $loggerStyle);
+        $this->assertInstanceOf(StyledLoggerDecorator::class, $styledLogger);
+    }
+
+    public function testDebug(): void
+    {
+        $this->styledLogger->debug(self::MESSAGE);
+        $this->expectOutputRegex(self::EXPECTED_REGEXP);
+    }
+
+    public function testInfo(): void
+    {
+        $this->styledLogger->info(self::MESSAGE);
+        $this->expectOutputRegex(self::EXPECTED_REGEXP);
+    }
+
+    public function testWarning(): void
+    {
+        $this->styledLogger->warning(self::MESSAGE);
+        $this->expectOutputRegex(self::EXPECTED_REGEXP);
+    }
+
+    public function testError(): void
+    {
+        $this->styledLogger->error(self::MESSAGE);
+        $this->expectOutputRegex(self::EXPECTED_REGEXP);
+    }
+
+    public function testCritical(): void
+    {
+        $this->styledLogger->critical(self::MESSAGE);
+        $this->expectOutputRegex(self::EXPECTED_REGEXP);
+    }
+}
